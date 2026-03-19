c57fb5b96b42679baa6b13bf2e04f5a727438d3d
nandhanaanilkumar <nandhanaanil2482@gmail.com>
2026-03-19 16:13:22 +0530

    Updated OTP using Resend API

--- Blogging-Backend/React-backend/server.js
+++ Blogging-Backend/React-backend/server.js
@@ -22,6 +22,7 @@ const ProfileView = require("./models/ProfileView");
 const app = express();
 const nodemailer=require("nodemailer");
 const PORT = process.env.PORT || 5000;
+import { Resend } from "resend";
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
@@ -1023,35 +1024,34 @@ app.get("/relationship/:viewerId/:profileId", async (req, res) => {
   res.json({ type: "none" });
 });
 
+
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 app.post("/send-otp", async (req, res) => {
-  const { email } = req.body;
+  try {
+    const { email } = req.body;
 
-  const otp = Math.floor(100000 + Math.random() * 900000).toString();
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
 
-  otpStore[email] = {
-    otp,
-    expiresAt: Date.now() + 3 * 60 *1000, // 60 seconds
-  };
+    otpStore[email] = {
+      otp,
+      expiresAt: Date.now() + 3 * 60 * 1000,
+    };
 
-  const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-      connectionTimeout: 10000,
-    });
-
-  await transporter.sendMail({
-      from: process.env.EMAIL_USER,
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
       to: email,
       subject: "Password Reset OTP",
       text: `Your OTP is: ${otp}`,
     });
 
-  res.json({ message: "OTP sent successfully" });
+    res.json({ message: "OTP sent successfully" });
+
+  } catch (error) {
+    console.error("RESEND ERROR:", error);
+    res.status(500).json({ error: "Failed to send OTP" });
+  }
 });
 app.post("/verify-otp", (req, res) => {
   const { email, otp } = req.body;
