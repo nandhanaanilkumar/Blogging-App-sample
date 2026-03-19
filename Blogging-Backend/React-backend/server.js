def2a50ab2035e56e7959c3149f8431b8f02c76a
nandhanaanilkumar <nandhanaanil2482@gmail.com>
2026-03-19 16:20:16 +0530

    Updated OTP using Resend API

--- Blogging-Backend/React-backend/server.js
+++ Blogging-Backend/React-backend/server.js
@@ -22,7 +22,7 @@ const ProfileView = require("./models/ProfileView");
 const app = express();
 const nodemailer=require("nodemailer");
 const PORT = process.env.PORT || 5000;
-import { Resend } from "resend";
+const { Resend } = require("resend");
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
@@ -1023,7 +1023,6 @@ app.get("/relationship/:viewerId/:profileId", async (req, res) => {
 
   res.json({ type: "none" });
 });
-
 
 
 const resend = new Resend(process.env.RESEND_API_KEY);
